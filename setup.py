4aec5b47ca46c60900e77ed2c644e8fb329a9905
apasquale <apasquale@237e6f29-e7ca-4063-98d9-6e1daa49f16a>
2010-05-31 14:27:52 +0000

    Initial import
    git-svn-id: https://cloudspring.svn.beanstalkapp.com/cloudspring_sfmembers/trunk@2 237e6f29-e7ca-4063-98d9-6e1daa49f16a

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='cloudspring.sfmembers',
+      version=version,
+      description="A project to synchronize SalesForce accounts and contacts with Plone.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Andrew Pasquale',
+      author_email='andrew@elytra.net',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['cloudspring'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+          'plone.app.dexterity',
+          'collective.autopermission',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
