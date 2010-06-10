9e34f0b9ae0cc29f50be68ea4db76c6aa5b1d5a8
apasquale <apasquale@237e6f29-e7ca-4063-98d9-6e1daa49f16a>
2010-06-10 00:51:53 +0000

    Updated tests for relations between affiliated members and organizations.
    git-svn-id: https://cloudspring.svn.beanstalkapp.com/cloudspring_sfmembers/trunk@8 237e6f29-e7ca-4063-98d9-6e1daa49f16a

--- cloudspring/sfmembers/tests/test_sfmembers.py
+++ cloudspring/sfmembers/tests/test_sfmembers.py
@@ -1,6 +1,6 @@
 import unittest
 from cloudspring.sfmembers.tests.base import IntegrationTestCase
-
+from cloudspring.sfmembers.member import MemberOrgs 
 # from example.dexteritypage.interfaces import ILayer
 # 
 # from zope.component import getMultiAdapter, queryMultiAdapter, getUtility
@@ -14,20 +14,45 @@ class TestContent(IntegrationTestCase):
         # Will raise an exception if we can't add this content
         self.folder.invokeFactory('cloudspring.sfmembers.member', 'member')
         self.folder.invokeFactory('cloudspring.sfmembers.organization', 'organization')
-    
+        
     def test_member_properties(self):
+         # Create a member object.
          self.folder.invokeFactory('cloudspring.sfmembers.member', 'm')
+
+         # Test the basic properties of a member object.
          m = getattr(self.folder, 'm')
          m.title = u"Title"
          m.description = u"Description"
          m.bio = u"Some text"
-         
+
          self.assertEquals(u"Title", m.Title())
          self.assertEquals(u"Description", m.Description())
          self.assertEquals(u"Some text", m.bio)
 
+         # Test the affiated organizations object.
+         # member.relatedOrganizations is a list of dict objects. 
+         from cloudspring.sfmembers.member import MemberOrgs
+         org1 = dict({"orgId": u"cloudspring", 
+                      "orgName": u"Cloudspring", 
+                      "role": u"developer"})
+         org2 = dict({"orgId": u"elytra", 
+                      "orgName": u"Elytra",
+                      "role": u"president"})
+
+         relOrg1 = MemberOrgs(org1)
+         relOrg2 = MemberOrgs(org2)
+         m.relatedOrganizations = [org1, org2]
+         
+         orgs = m.relatedOrganizations
+         self.assertEquals(u"cloudspring", orgs[0]["orgId"])
+         self.assertEquals(u"president",orgs[1]["role"])
+         
+
     def test_organization_properties(self):
+         # Create a organization object.
          self.folder.invokeFactory('cloudspring.sfmembers.organization', 'o')
+
+         # Test the basic organization properties.
          o = getattr(self.folder, 'o')
          o.title = u"Title"
          o.description = u"Description"
@@ -35,8 +60,26 @@ class TestContent(IntegrationTestCase):
          
          self.assertEquals(u"Title", o.Title())
          self.assertEquals(u"Description", o.Description())
-         self.assertEquals(u"Some text", o.detailedDescription())
+         self.assertEquals(u"Some text", o.detailedDescription)
     
+         # Test the affiliated members object.
+         # organization.relatedMembers is a list of dict objects. 
+         from cloudspring.sfmembers.organization import OrgMembers
+         member1 = dict({"memberId": u"apasquale", 
+                      "memberName": u"Andrew Pasquale", 
+                      "role": u"developer"})
+         member2 = dict({"memberId": u"khomstead", 
+                      "memberName": u"Kyle Homstead",
+                      "role": u"president"})
+
+         relMember1 = OrgMembers(member1)
+         relMember2 = OrgMembers(member2)
+         o.relatedMembers = [member1, member2]
+         
+         members = o.relatedMembers
+         self.assertEquals(u"apasquale", members[0]["memberId"])
+         self.assertEquals(u"Kyle Homstead", members[1]["memberName"])
+    # def test_news_item_schema(self):
     # def test_news_item_schema(self):
     #     self.folder.invokeFactory('News Item', 'n1')
     #     n1 = getattr(self.folder, 'n1')
