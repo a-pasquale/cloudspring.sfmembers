02a3a47de4aaab140250481ff031333b48def1d2
apasquale <apasquale@237e6f29-e7ca-4063-98d9-6e1daa49f16a>
2011-01-17 15:49:51 +0000

    Added assignment number field to blog type and enabled indexing for collections.
    git-svn-id: https://cloudspring.svn.beanstalkapp.com/cloudspring_sfmembers/branches/center.groundshift.org@82 237e6f29-e7ca-4063-98d9-6e1daa49f16a

--- cloudspring/sfmembers/extender.py
+++ cloudspring/sfmembers/extender.py
@@ -3,10 +3,15 @@ from zope.component import adapts
 from zope.interface import implements
 from zope import schema
 from archetypes.schemaextender.interfaces import ISchemaExtender
-from Products.Archetypes.atapi import StringField, SelectionWidget
+from Products.Archetypes.atapi import StringField, SelectionWidget, StringWidget
 from Products.ATContentTypes.content.newsitem import ATNewsItem
 from cloudspring.sfmembers import _
+
+
 class BlogTypeField(ExtensionField, StringField):
+    pass
+
+class AssignmentNumberField(ExtensionField, StringField):
     pass
 
 class BlogExtender(object):
@@ -16,11 +21,18 @@ class BlogExtender(object):
 
     fields = [ 
         BlogTypeField(name='BlogTypeField',
-                  vocabulary=[_(u'Pre-class reflection'), _(u'Post-class reflection'), _(u'Seminar feed'), _(u'Personal blog')],
+                  vocabulary=[_(u'Assignment'),_(u'Pre-class reflection'), _(u'Post-class reflection'), _(u'Seminar feed'), _(u'Personal blog')],
                   widget=SelectionWidget(
                       label=_(u'Blog type:'),
                       format='radio'
                   ),
+        ),
+        AssignmentNumberField(
+            name=_(u"AssignmentNumberField"),
+            widget=StringWidget(
+                label=_(u"Assignment number:"),            
+                description=_(u"Enter the relevant class number."),
+            ),
         ),
     ]
  
