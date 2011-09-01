7f902077a1dfefdabd30b19ae1a4ab8f1105bcf1
Andrew Pasquale <andrew@elytra.net>
2011-09-01 11:26:26 -0400

    Removed move collective.gtags stuff.

--- cloudspring/sfmembers/reflection.py
+++ cloudspring/sfmembers/reflection.py
@@ -2,43 +2,16 @@ from five import grok
 from zope import schema
 from plone.directives import form, dexterity
 from plone.app.textfield import RichText
-from zope.interface import implements
-from plone.behavior.interfaces import IBehaviorAssignable
-from collective.gtags.behaviors import ITags
-from zope.component import adapts
-from zope.component import provideAdapter
 from cloudspring.sfmembers import _
 
 class IReflection(form.Schema):
     """  A reflection
     """
 
-    title = schema.TextLine(
-            title=_(u"Title"),
-            )
-
     text = RichText(
             title=_(u"Text"),
             )
     
-
-#class Reflection(object):
-#    implements(IBehaviorAssignable)
-#    adapts(IReflection)
-#
-#    enabled = [ITags]
-#
-#    def __init__(self, context):
-#        self.context = context
-#        
-#    def supports(self, behavior_interface):
-#        return behavior_interface in self.enabled
-#    
-#    def enumerate_behaviors(self):
-#        for e in self.enabled:
-#            yield queryUtility(IBehavior, name=e.__identifier__)
-#
-#provideAdapter(ReflectionAssignable)
 
 class blog_item_view(grok.View):
     grok.context(IReflection)
